b65e9dc406c443c52923d0e637b98e584a7d8af3
dorin.ilea <ilea_dorin22@yahoo.com>
2024-08-12 08:55:54 +0300

    works fine, need to make it prettier

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ let result = 0;
 let clear = true;
 let acc = 0;
 
-let display = document.querySelector('#display');
+let display = document.querySelector('h2');
+
 
 function setOperation(op) {
     operator = op;
@@ -15,7 +16,7 @@ function setOperation(op) {
 
 function clearDisplay() {
     clear = true;
-    display.value = '';
+    display.textContent = '';
     op.left = '';
     op.right = '';
     acc = 0;
@@ -33,9 +34,45 @@ function appendNumber(num) {
 }
 
 function setOperation(oper) {
+    if(op.left === '') {
+        return;
+    }
+    if(op.right !== '') {
+        calculate();
+    }
     operator = oper;
     clear = false;
     display.textContent += oper;
+}
+
+function calculate() {
+    if(op.right === '' || op.left === '') {
+        return;
+    }
+    let left = parseFloat(op.left);
+    let right = parseFloat(op.right);
+    switch (operator) {
+        case '+':
+            result = left + right;
+            break;
+        case '-':
+            result = left - right;
+            break;
+        case '*':
+            result = left * right;
+            break;
+        case '/':
+            if(right === 0) {
+                display.textContent = 'illegal operation, you go yo jail!';
+                return;
+            }
+            result = left / right;
+            break;
+    }
+    display.textContent = result;
+    acc = result;
+    op.left = result;
+    op.right = '';
 }
 
 
